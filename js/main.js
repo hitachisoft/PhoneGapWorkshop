925bff77f43c484d68217b0965e1479a6c550e2e
hitachisoft <d-morris@hitachisolutions-eu.com>
2013-07-02 12:16:55 +0100

    add view class

--- js/main.js
+++ js/main.js
@@ -1,36 +1,19 @@
-/*globals console, $, MemoryStore, alert, Handlebars*/
-
 var app = {
 
-	findByName: function () {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function (employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
-	initialize: function () {
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-		var self = this;
-		this.store = new MemoryStore(function () {
-			self.renderHomeView();
-		});
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-	renderHomeView: function () {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-	showAlert: function (message, title) {
-		if (navigator.notification) {
-			navigator.notification.alert(message, null, title, "OK");
-		} else {
-			alert(title ? (title + ": " + message) : message);
-		}
-	}
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+    }
 
 };
 
