35f414f80bc830cd6dfa09f851a2b5efa502726a
hitachisoft <d-morris@hitachisolutions-eu.com>
2013-07-02 11:13:50 +0100

    test native dialogs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/*globals console, $, MemoryStore, alert*/
+
 var app = {
 
     findByName: function() {
@@ -6,17 +8,28 @@ var app = {
             var l = employees.length;
             var e;
             $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
+            for (var i=0; i<l; i+=1) {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+	initialize: function () {
+		var self = this;
+		this.store = new MemoryStore(function () {
+			self.showAlert("Store Initialized", "Info");
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, "OK");
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	}
 
 };
 
