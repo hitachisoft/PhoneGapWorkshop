27a30077210e733a85d3211c403f6ee48113f566
hitachisoft <d-morris@hitachisolutions-eu.com>
2013-07-02 14:34:45 +0100

    update to end of geolocation

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,4 +1,4 @@
-var HomeView = function(store) {
+﻿var HomeView = function(store) {
 
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
@@ -11,10 +11,17 @@ var HomeView = function(store) {
         return this;
     };
 
-    this.findByName = function() {
-        store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(HomeView.liTemplate(employees));
-        });
+    this.findByName = function () {
+    	store.findByName($('.search-key').val(), function (employees) {
+    		$('.employee-list').html(HomeView.liTemplate(employees));
+    		if (self.iscroll) {
+    			console.log('Refresh iScroll');
+    			self.iscroll.refresh();
+    		} else {
+    			console.log('New iScroll');
+    			self.iscroll = new iScroll($('.scroll', self.el)[0], { hScrollbar: false, vScrollbar: false });
+    		}
+    	});
     };
 
     this.initialize();
